b859d72c139e6394a177c0a9d206bbade049f2dd
Samuel <samuellegg2004@icloud.com>
2022-01-21 17:26:04 +0000

    -created constructor for QueueClass

--- EndOfChapter27/Q3.py
+++ EndOfChapter27/Q3.py
@@ -3,3 +3,29 @@
 #Part A
 # QueueClass is a derived class of the abstract class NodeClass
 
+#Part B
+class NodeClass:
+    def __init__(self):
+        self.__data : str = ""
+        self.__pointer : int = -1
+
+    def SetData(self, data : str):
+        self.__data = data
+    
+    def SetPointer(self, pointer : int):
+        self.__pointer = pointer
+
+    def GetData(self) -> str:
+        return self.__data
+    
+    def GetPointer(self) -> int:
+        return self.__pointer
+
+
+class QueueClass:
+    def __init__(self):
+        self.__queue = [NodeClass for _ in range(50)]
+        self.__head : int = -1
+        self.__tail : int = -1
+
+        
